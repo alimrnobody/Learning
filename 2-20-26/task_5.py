5209cc6717a1c4a769d3865d6bf94791c0b8e4a0
unknown <romanbwp876@gmail.com>
2026-02-25 12:14:31 +0500

    daily practice

--- 2-20-26/task_5.py
+++ 2-20-26/task_5.py
@@ -1,1 +1,11 @@
 # 🔥 Next Level (Week 2 Real Thinking)
+
+for i in range(1, 6):
+    for j in range(i):
+        print("*", end="")
+    print()    
+
+for q in range(4, 0, -1):
+    for p in range(q):
+        print("*", end="")
+    print()    
---
+++ 2-20-26/task_6.py
@@ -0,0 +1,8 @@
+# 🔥 Task – Smallest Unique Number
+
+nums = [3,5,3,7,9,5,1]
+random = []
+
+for i in nums:
+    for j in random:
+        print(j)
